45a014ba0e21fde93c7e9367a3d6cf3626a21eee
Jeong Dong <eejxdong99@gmail.com>
2022-09-27 23:35:58 +0900

    project1: 기본기능 구현

---
+++ project1/jeongdong_reg/src/main/java/tukorea/websvc/club/persistence/StudentDAO.java
@@ -0,0 +1,18 @@
+package tukorea.websvc.club.persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+import tukorea.websvc.club.domain.*;
+
+public class StudentDAO {
+	private static Map<String, StudentVO> storage = new HashMap<String, StudentVO>();
+	
+	public boolean join (StudentVO student) {
+		try {
+			storage.put(student.getId(), student);
+		} catch(Exception e) {
+			return false;
+		}
+		return true;
+	}
+}
